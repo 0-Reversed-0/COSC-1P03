8aa2a7b20de1776b4fbda45f443eb1d21369a6f6
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-05-05 13:55:30 -0400

    Added Lecture 2

--- Lectures/Lecture_1/src/Main.java
+++ Lectures/Lecture_1/src/Main.java
@@ -1,7 +1,33 @@
+import java.*;
+
 public class Main
 {
+
+    public Main() // a constructor contracts an instance of the class
+    {
+        someMethod();
+    }
+
+    private void nope()
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        double mean = (a+b+c+d+e)/5.0;
+        System.out.println(mean); 
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        new Main();
     }
+
+    void someMethod() // the only way to not attach static is to put the method in a Constructor
+    {
+        System.out.println("Hi");
+    }
+
 }
