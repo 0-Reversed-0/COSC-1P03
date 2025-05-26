2cb707a1fe9167aebf59082aa709e5b8b4332335
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-05-25 23:32:44 -0400

    code

---
+++ Lectures/Lecture_6/src/L5/StopTrollingException.java
@@ -0,0 +1,10 @@
+package L5;
+
+/**
+ * Runtime exception is basically an unchecked exception
+ */
+
+public class StopTrollingException extends RuntimeException //basically says that a Stop Trolling exception is a Runtime exception
+{
+    // It does not need anything else
+}
