eb3e2565fb68d959a594699e9f8d9f0735deae85
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-05-26 14:27:16 -0400

    code

--- Lectures/Lecture_7/src/Main.java
+++ Lectures/Lecture_7/src/Main.java
@@ -1,7 +1,67 @@
+/**
+ * This lecture is the introduction to Linked Lists.
+ */
+
 public class Main
 {
+
+    /*
+     * Storing data is challenging with arrays since we have a static amount of space
+     * We want a data structure (course name mentioned!!!) that is resizable
+     * We also want to have something that is easy to insert values in the middle
+     * Our solution would be a Dynamic Structure
+     */
+
+    public Main()
+    {
+        theProblem();
+
+        /*
+         * The Solution would be something called a Linked List
+         * A linked list would be a collection of Nodes that are sequentially linked
+         * Nodes: An object that stores a pointer and an item(any type of data)
+         * Pointer: A pointer is a reference to a Node/Null to sequentially link a list
+         * Sequentially Linked: Objects with references that point to the next object until it hits null
+         */
+
+        hardCodedLinkedList();
+    }
+
+    private void hardCodedLinkedList()
+    {
+        Node head;                          // The head is the starting point of the Linked List
+        head = new Node(14, null);   // The head is basically starting the list with 14 out alone pointing to nothing
+        head = new Node(7, head);        // Then the new head starts the list with 7 then points to the old head
+        head = new Node(3, head);        // Think about it like pushing a bunch of blocks(Nodes in our case)
+        head = new Node(1, head);        // and then adding a new block in the front of the blocks
+
+        Node ptr = head; // To traverse this horrible list, we can use a temporary pointer Node that points to head
+
+        while (ptr != null) //then we make it so that if the POINTER POINTS TO NULL, THEN WE STOP THE TRAVERSING!!!!
+        {
+            System.out.print(ptr.item + " -> "); //then we take the item out of the pointer so we can display it
+            ptr = ptr.next; //then move on the next node
+        }
+
+    }
+
+    private void theProblem()
+    {
+        int[] arr = new int[8];
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = i;
+        }
+
+        // If I wanted to add let's say the 9th element,
+        // then I would have to make a whole new array just add a new element
+        // If I wanted to add something in the middle at arr[3] would have me end up copying the array AND shifting the values along
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        new Main();
     }
+
 }
---
+++ Lectures/Lecture_7/src/Node.java
@@ -0,0 +1,17 @@
+public class Node
+{
+
+    /**
+     * To start making a Dynamically Linked Structure AKA a Linked Lists, we need to make a Node class
+     * The node class is a wrapper class that is any object that modify the usage of another (basically an esay way to access our data)
+     */
+
+    int item; // This is what the Linked List's node shall hold
+    Node next; // This is the reference to the next node
+
+    Node(int n, Node nx) //This is class is package private (only visible and accessible in the same package). All node classes SHOULD BE package private
+    {
+        item = n; // We put what integer we want to store in the Node,
+        next = nx; // then we add what we would like to point it to (could be another Node or null)
+    }
+}
