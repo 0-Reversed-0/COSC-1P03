39d05efd377fedb240f08462efcdce251f07d319
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-06-02 02:57:05 -0400

    Updated Lab 4

--- Labs/Lab_4/src/construction/List.java
+++ Labs/Lab_4/src/construction/List.java
@@ -1,6 +1,65 @@
+/**
+ * Author: Reversed_0
+ * Course: COSC 1P03
+ * Title: Linked Lists Lab
+ */
+
 package construction;
+
+/*
+ * Just a heads-up for anyone who is actually looking at this to study,
+ * THIS IS NOT HOW YOU SHOULD CODE A LINKED LIST WHATSOEVER REFER TO MY LECTURE 8 NOTES FOR THAT PLEASE!!!!!
+ */
 
 public class List
 {
+    Node head = null;
 
+    public List()
+    {
+        /*
+         * The First part can be completely ignored honestly just show your able to convert an array to a list and traverse a list
+         */
+
+        int[] arr = {6, 8, 8, 5, 5, 5, 0};
+
+        toList(arr); // this SHOULD be a void method if we do not have any sort of interface to work with and are hardcoding everything
+
+        display();
+    }
+
+    /**
+     * A method that converts an array to a list
+     * @param arr the array ready to be converted
+     */
+
+    private void toList(int[] arr)
+    {
+        for (int i = arr.length-1; i >= 0; i--) // since link lists add values from right to left, we have to reverse the way we read the array
+        {
+            head = new Node(arr[i], head); // we point to head because right now the new node is the new head pointing to our old head which is null (everything works!!!)
+        }
+    }
+
+    /**
+     * Displayer method to display our linked list
+     */
+
+    private void display()
+    {
+        Node ptr = head; // first make a pointer node that is set to head(DO NOT USE THE HEAD REFERENCE TO TRAVERSE ANY LINKED LIST WE ONLY USE HEAD TO ADD NODES)
+
+        while (ptr != null) // to make sure we do not get a null pointer exception we make sure that we do not continue if the current node we are at is null
+        {
+            System.out.print(ptr.item + " -> "); // call the item in the node object currently and add a silly arrow beside it
+            ptr = ptr.next; // make the current node we are (example: head) equal to the node in front of it
+        }
+
+        System.out.print("null"); // for formatting purposes only
+    }
+
+    public static void main(String[] args)
+    {
+        new List();
+    }
 }
--- Labs/Lab_4/src/construction/Node.java
+++ Labs/Lab_4/src/construction/Node.java
@@ -2,12 +2,12 @@ package construction;
 
 public class Node
 {
-    int item;
-    Node next;
+    int item; // Nodes are what stores the values in a linked list so it makes sense why we have an item that the object will hold
+    Node next; // this is our link to all the other nodes
 
     Node(int I, Node N) // remember that nodes are package private!!!
     {
-        item = I;
-        next = N;
+        item = I; // Now the user can set the item to be whatever they like
+        next = N; // Every node HAS TO HAVE a reference to another Node whether it is a random node our the actual next node
     }
 }
