b7187b7192414c46bbaab941d13f747a8afb7af7
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-06-17 16:19:01 -0400

    Updated Lecture 12

---
+++ Lectures/Lecture_12/src/Main.java
@@ -0,0 +1,39 @@
+/**
+ * Recursion is a function that calls itself until it does not need to.
+ */
+
+public class Main
+{
+    
+    public Main()
+    {
+        int a = factorial(3);
+        System.out.println(a);
+    }
+    
+    public void main(String[] args)
+    {
+        new Main();
+    }
+
+    int factorial(int n)
+    {
+        if (n < 1) // this is our base case meaning it will stop here
+        {
+            return 1; // stops the recursion here since we would return one and not call the function upon itself
+        }
+
+        return n * factorial(n - 1); // this would be the guaranteed progress to the 1 since n has to decrement
+    }
+
+
+
+    int fib(int n)
+    {
+        if (n < 2)
+        {
+            return n;
+        }
+        return fib(n - 2) + fib(n - 1);
+    }
+}
