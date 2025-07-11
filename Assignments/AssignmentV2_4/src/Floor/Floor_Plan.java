87ec8ed3e097ae3229ad9a27a33ff1507cfa2567
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-07-10 22:46:15 -0400

    Updated Assignment 4

--- Assignments/AssignmentV2_4/src/Floor/Floor_Plan.java
+++ Assignments/AssignmentV2_4/src/Floor/Floor_Plan.java
@@ -10,14 +10,16 @@ package Floor;
 /**
  * This is the recursive class of the Floor plan generator.
  * Floor_Plan handles doing the math and logistics behind making the actual floor plan
- * This is also the class the client handles with and where the client inputs the dimensions
+ * This is also the class the client handles with and where the client inputs the dimensions, cutoff, and stop chance
+ * It commands the Draw class to draw it for it.
  */
 
 public class Floor_Plan
 {
 
     private Draw draw;
-    private final int cutoff = 1; // We hardcode these values since we want to have these conditions static regardless of what passed in the constructor
+    private final int cutoff;
+    private final double stop;
 
     /**
      * Creates a new object to draw everything needed and immediately runs the recursive solution once values are passed
@@ -25,18 +27,24 @@ public class Floor_Plan
      * @param cellSize is measurement of how long and wide each square cell is on one side
      * @param rows     is the amount of rows of cells will the floor plan have
      * @param cols     is the amount of columns of cells will the floor plan have
+     * @param cut      is the minimum amount of cells that can be in one singular room
+     * @param S        is the stop probability of the program stopping randomly rather just based off cutoff
      */
 
-    public Floor_Plan(int cellSize, int rows, int cols)
+    public Floor_Plan(int cellSize, int rows, int cols, int cut, int S)
     {
         draw = new Draw(cellSize, rows, cols);
+        cutoff = cut;
+        stop = (double) S / 100; // I divide stop by 100 just to make it easier to compare it to Math.random
 
-        floorCreation(rows, cols, 0, 0);
+        floorCreation(rows, cols, 0, 0); // we pass in (0,0) since every canvas has to start at (0,0) with how our grid system was coded
     }
 
 
     /**
      * Recursive method that handles the logistics behind painting, creating walls, where to put walls, and when to stop
+     * This recursing method takes advantage of the Draw classes' grid system to calculate and compare on whether to keep cutting with walls or not
+     * The algorithm works via taking a big floor and splitting it into multiple smaller floors until the cutoff is hit, and we then add all the walls stored in the call stack
      *
      * @param endX   the amount of horizontal cells left in the recursive solution (the arbitrary granularity)
      * @param endY   the amount of vertical cells left in the recursive solution
@@ -44,44 +52,72 @@ public class Floor_Plan
      * @param startY the start of where the cells will be in the Y axis
      */
 
-    private void floorCreation(int endX, int endY, int startX, int startY)
+    private void floorCreation(int endX, int endY, int startX, int startY) // IMPORTANT TO NOTE: that I am stupid and decided the ending coordinates before the starting ones so PLEASE IN MIND (END, END) <- (START, START)
     {
-        int stopChance = randomPercent(2);
-
         int width = endX - startX;
-        int height = endY - startY;
+        int height = endY - startY; // we do this because there can be pixels in coordinates like (4131243, 491212) that have less space than the cutoff demands so to accommodate we scale it down by subtracting from it
 
         // Base case:
-        if (stopChance == 3 || ((width <= cutoff) & (height <= cutoff))) // if the stop chance occurs or if rows/cols is under the cutoff then...
-        {
-            draw.paint(startX, startY, endX, endY);
 
-            return; // then stop the recursion
+        /*
+         * The base case has 2 condition that stop the recursion
+         * 1) If the width and the height of what is being cut is lesser than or equal to the cutoff then stop recursing
+         * 2) Or if, on the rare chance, the stop probability is hit then stop recursing as well
+         */
+
+        if ((width <= cutoff & height <= cutoff) || (Math.random() <= stop))
+        {
+            if (Math.random() <= 0.15) // We don't want paint on every single stop so we make paint come 15% of the time
+            {
+                draw.paint(startX, startY, endX, endY);
+                return; // stop the recursion process
+            }
         }
 
         // Recursive solution:
-        if (endX == endY || endX > endY) // if we have a perfect square floor then we just cut it with a vertical line or if we have a wide room then
+
+        if (width >= height) // since squares are considered wide we would cut them vertically since Earl Foxwell said so
         {
-            int randX = randomCell(endX - 1, startX + 1); // helps us avoids the borders of the canvas
+            if (width <= cutoff) // this is a safeguard before any movement on turtle is made
+            {
+                return;
+            }
 
-            draw.createWall_V(startY, endY, randX);
+            int randX = randomCell(endX - 1, startX + 1); // we need a random spot on the grid to make the new wall
 
-            floorCreation(randX, endY, startX, startY);
-            floorCreation(startX, startY, randX, endY);
+            draw.createWall_V(startY, endY, randX); // the wall would extend from the starting coordinate to the ending coordinate and originate on random X cell we have chosen
 
-        } else if (endX < endY) // if the floor we are dealing with is too tall then we would cut horizontally
+            /*
+             * The recursive step of this section of this problem involves 2 method calls
+             *
+             * The first method call handles everything from the LEFT of the newly created wall so (0,0) -> (random #, Max Y value) [NOTE: 0,0 is a placeholder for the starting x and y]
+             * It will place more and more walls of until one of the 3 base cases are met. (wall specific cutoff call v.s. main base case)
+             *
+             * The second method call handles everything from thr RIGHT of the first wall placed so (the same random #, 0) -> (max X, max Y)
+             * The same base cases apply to it as well of course
+             */
+
+            floorCreation(randX, endY, startX, startY); // LEFT
+            floorCreation(endX, endY, randX, startY);   // RIGHT
+
+        } else // if it's not wide or square then, by process of elimination, it has to be tall meaning we would cut them horizontally
         {
+            if (height <= cutoff)
+            {
+                return;
+            }
+
             int randY = randomCell(endY - 1, startY + 1);
 
             draw.createWall_H(startX, endX, randY);
 
-            floorCreation(endX, randY, startX, startY);
-            floorCreation(startX, startY, endX, randY);
+            floorCreation(endX, randY, startX, startY); // LEFT  : (starting point x,y) -> (MAX X VALUE, random #)
+            floorCreation(endX, endY, startX, randY);   // RIGHT : (same random #, 0) -> (Max X,Y)
         }
     }
 
     /**
-     * A method to get a cell to draw a wall on
+     * A method to get a random cell to be used to determine where to place a wall on the floor
      *
      * @param max is the further the cell can go
      * @param min is the least the cell can go
@@ -91,16 +127,5 @@ public class Floor_Plan
     private int randomCell(int max, int min)
     {
         return (int) ((max - min + 1) * Math.random() + min);
-    }
-
-    /**
-     * Method to get random percentages for the random stop chance that will stop the recursive solution or for painting
-     *
-     * @return a random percentage
-     */
-
-    private int randomPercent(int factor)
-    {
-        return (int) (((double) 100 / factor) * Math.random());
     }
 }
--- Assignments/AssignmentV2_4/src/client/Client.java
+++ Assignments/AssignmentV2_4/src/client/Client.java
@@ -15,6 +15,7 @@ import java.util.Scanner; // I hate BasicIO so much, so I'm using Scanner instea
  * The client class is the only executable class in the entire project
  * It's main purpose is to test the capabilities of the Floor_Plan class
  * The testing will be up to the user via inputting different row : columns ratios and different values of the cell size
+ * I used Scanner over BasicIO because Earl approves of its use
  */
 
 public class Client
@@ -23,7 +24,27 @@ public class Client
 
     public Client()
     {
-        Floor_Plan floor = new Floor_Plan(60, 5, 5);
+        userTest();
+    }
+
+    /**
+     * Gathers information through the Scanner object to pass the certain items through the constructor.
+     */
+
+    private void userTest()
+    {
+        System.out.print("\nInput the ratio at which the cell will sized in Turtle pixels: ");
+        int cellSize = input.nextInt();
+        System.out.print("\nInput the amount of rows of cells will be on the floor: ");
+        int rows = input.nextInt();
+        System.out.print("\nInput the amount of columns of cells will be on the floor: ");
+        int columns = input.nextInt();
+        System.out.print("\nInput the cutoff for when cells are too small: ");
+        int cutoff = input.nextInt();
+        System.out.print("\nInput the stop probability of the program ending on its on (in whole number form): %");
+        int stopProb = input.nextInt();
+
+        Floor_Plan floor = new Floor_Plan(cellSize, rows, columns, cutoff, stopProb);
     }
 
     public static void main(String[] args)
