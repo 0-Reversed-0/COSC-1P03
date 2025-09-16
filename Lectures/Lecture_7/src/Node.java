677fc27211d767520322f3ec01668ec0a88988a7
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-09-16 03:06:26 -0400

    Introduction to Linked Lists

--- Lectures/Lecture_7/src/Node.java
+++ Lectures/Lecture_7/src/Node.java
@@ -1,17 +1,23 @@
+/**
+ * This is a basic introduction into linked list
+ * The implementation of actual linked list classes are shown in Lecture_8
+ */
+
 public class Node
 {
 
     /**
      * To start making a Dynamically Linked Structure AKA a Linked Lists, we need to make a Node class
-     * The node class is a wrapper class that is any object that modify the usage of another (basically an easy way to access our data)
+     * The node class is a wrapper class
+     * A wrapper class is an object that holds primitive types sort of like wrapping an object around a primitive type (Generics)
      */
 
     int item; // This is what the Linked List's node shall hold
     Node next; // This is the reference to the next node
 
-    Node(int n, Node nx) //This is class is package private (only visible and accessible in the same package). All node classes SHOULD BE package private
+    Node(int i, Node nx) //This is class is package private (only visible and accessible in the same package). All node classes SHOULD BE package private
     {
-        item = n; // We put what integer we want to store in the Node,
+        item = i; // We put what integer we want to store in the Node,
         next = nx; // then we add what we would like to point it to (could be another Node or null)
     }
 }
